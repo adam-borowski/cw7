d1dd7e93ad3701695766ac3122fa2fd071bbd0bb
Adam <adam@hp.probook>
2021-02-07 17:19:22 +0100

    Login endpoint

--- Controllers/EnrollmentsController.cs
+++ Controllers/EnrollmentsController.cs
@@ -1,16 +1,26 @@
 using Cw7.Services;
 using Cw7.DTOs.Requests;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System;
 
 namespace Cw7.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("api/enrollments")]
     public class EnrollmentsController : ControllerBase {
         private readonly IStudentDbService _dbService;
+        public IConfiguration Configuration { get; }
 
-        public EnrollmentsController(IStudentDbService dbService) {
+        public EnrollmentsController(IStudentDbService dbService, IConfiguration configuration) {
             _dbService = dbService;
+            Configuration = configuration;
         }
 
         [HttpPost]
@@ -30,6 +40,33 @@ namespace Cw7.Controllers
             }
 
             return Created("http://localhost:5001", response);
+        }
+
+        [HttpPost]
+        public IActionResult Login() {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "Jan"),
+                new Claim(ClaimTypes.Role, "student")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Secret Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken
+            (
+                issuer: "gakko",
+                audience: "students",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+            );
+
+            return Ok(new 
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = Guid.NewGuid()
+            });
         }
     }
 }
---
+++ DTOs/Requests/LoginRequest.cs
@@ -0,0 +1,4 @@
+class LoginRequest {
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
